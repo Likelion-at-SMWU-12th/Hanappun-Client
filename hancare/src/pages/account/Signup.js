d77370cd6945c1fa3f83fbfb0714d8c8d339ee58
hyhy-j <lhjlhj10088@gmail.com>
2024-07-26 02:47:42 +0900

    [Chore] 창 크기 관련 App.js 코드 수정

--- hancare/src/pages/account/Signup.js
+++ hancare/src/pages/account/Signup.js
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.div`
+  text-align: center;
+  margin-top: 40px;
+  font-size: 15px;
+  font-weight: bolder;
+  color: #7350ff;
+  margin-bottom: 30px;
+`;
+
+const Infobox = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 50px;
+`;
+
+const Label = styled.label`
+  font-size: 17px;
+  font-weight: bolder;
+`;
+
+const Inputbox = styled.input`
+  margin-top: 10px;
+  margin-right: 50px;
+  padding: 15px;
+  background-color: #eceef0;
+  font-weight: bolder;
+  border: none;
+  border-radius: 10px;
+`;
 
 const Signup = () => {
-  return <div></div>;
+  const [values, setValues] = useState({
+    nickname: "",
+    id: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setValues((prevValues) => {
+      const { name, value } = e.target;
+      return {
+        ...prevValues,
+        [name]: value,
+      };
+    });
+  };
+  return (
+    <Container>
+      <Title>
+        <h1>회원가입</h1>
+      </Title>
+      <form>
+        <Infobox>
+          <Label for="nickname">닉네임</Label>
+          <Inputbox
+            type="text"
+            name="nickname"
+            value={values.nickname}
+            onChange={handleChange}
+            placeholder="닉네임을 입력하세요"
+          />
+        </Infobox>
+      </form>
+    </Container>
+  );
 };
 
 export default Signup;
