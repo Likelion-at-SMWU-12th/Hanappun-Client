243640f3c3ba236f63ac9a95eed08bc3c74d2bb0
uehddms <ehddms3@sookmyung.ac.kr>
2024-07-29 03:30:06 +0900

    [Add] pages 페이지 생성, [Chore] 생성한 페이지 Add.js라우터 설정

--- hancare/src/App.js
+++ hancare/src/App.js
@@ -4,17 +4,19 @@ import styled from "styled-components";
 import Startpage from "./pages/account/Startpage";
 import Signup from "./pages/account/Signup";
 import Login from "./pages/account/Login";
+import ColumnMain from "./pages/Columns/ColumnMain";
+import Profile from "./pages/mypage/Profile";
+import Cancel from "./pages/mypage/Cancel";
 
 const Wrapper = styled.div`
   width: 400px;
   height: 100vh;
   min-height: 844px;
   margin: 0 auto;
-  display: flex;
   box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.3),
     0 8px 10px -6px rgba(0, 0, 0, 0.3);
-  display: flex;
-  justify-content: center;
+  /*display: flex;
+  justify-content: center;*/
 `;
 
 function App() {
@@ -25,6 +27,9 @@ function App() {
         <Route path="/" element={<Startpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/column" element={<ColumnMain />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cancel" element={<Cancel />} />
       </Routes>
     </Wrapper>
   );
