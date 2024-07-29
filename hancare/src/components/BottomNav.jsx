9ae16152f2a44f0815f6f533ecfaa4b041884e07
uehddms <ehddms3@sookmyung.ac.kr>
2024-07-30 01:57:04 +0900

    [Chore] 원하는 url에 navbar 표시하도록 수정(url 추가 필요)

--- hancare/src/components/BottomNav.jsx
+++ hancare/src/components/BottomNav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./BottomNav.css";
-import { Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 // FontAwesomIcon 컴포넌트를 사용하기 위해 import
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //사용할 아이콘 import
@@ -13,57 +13,82 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 const BottomNav = () => {
   //현재 선택된 아이콘을 관리하는 state
   const [activeNav, setActiveNav] = useState(1);
-  return (
-    <nav className="wrapper">
-      {/*하단 네비게이션 최상위 태그*/}
-      {/* 나의 케어 */}
-      <Link to="/" className="nav-link" onClick={() => setActiveNav(1)}>
-        <div>
-          <FontAwesomeIcon
-            icon={faShieldHeart}
-            className={activeNav === 1 ? "nav-item active" : "nav-item"}
-          />
-        </div>
-      </Link>
-      {/* 우리 케어 */}
 
-      <Link to="/login" className="nav-link" onClick={() => setActiveNav(2)}>
-        <div>
-          <FontAwesomeIcon
-            icon={faHandHoldingHeart}
-            className={activeNav === 2 ? "nav-item active" : "nav-item"}
-          />
-        </div>
-      </Link>
-      {/* 한의원 (지도) */}
-      <Link to="/map" className="nav-link" onClick={() => setActiveNav(3)}>
-        <div>
-          <FontAwesomeIcon
-            icon={faLocationDot}
-            className={activeNav === 3 ? "nav-item active" : "nav-item"}
-          />
-        </div>
-      </Link>
-      {/* 칼럼 */}
-      <Link to="/column" className="nav-link" onClick={() => setActiveNav(4)}>
-        <div>
-          <FontAwesomeIcon
-            icon={faFileLines}
-            className={activeNav === 4 ? "nav-item active" : "nav-item"}
-          />
-        </div>
-      </Link>
-      {/* MY */}
-      <Link to="/profile" className="nav-link" onClick={() => setActiveNav(5)}>
-        <div>
-          <FontAwesomeIcon
-            icon={faUser}
-            className={activeNav === 5 ? "nav-item active" : "nav-item"}
-          />
-        </div>
-      </Link>
-    </nav>
-  );
+  //현재 url
+  const locationNow = useLocation();
+
+  if (
+    locationNow.pathname === "/" ||
+    locationNow.pathname === "/column" ||
+    locationNow.pathname === "/mypage" ||
+    locationNow.pathname === "/profile" ||
+    locationNow.pathname === "/map" ||
+    locationNow.pathname === "/cancel"
+    //navbar를 표시할 url 작성
+  ) {
+    return (
+      <nav className="wrapper">
+        {/*하단 네비게이션 최상위 태그*/}
+        {/* 나의 케어 */}
+        <Link to="/" className="nav-link" onClick={() => setActiveNav(1)}>
+          <div>
+            <FontAwesomeIcon
+              icon={faShieldHeart}
+              size="lg"
+              className={activeNav === 1 ? "nav-item active" : "nav-item"}
+            />
+          </div>
+        </Link>
+        {/* 우리 케어 */}
+
+        <Link to="/login" className="nav-link" onClick={() => setActiveNav(2)}>
+          <div>
+            <FontAwesomeIcon
+              icon={faHandHoldingHeart}
+              size="lg"
+              className={activeNav === 2 ? "nav-item active" : "nav-item"}
+            />
+          </div>
+        </Link>
+        {/* 한의원 (지도) */}
+        <Link to="/map" className="nav-link" onClick={() => setActiveNav(3)}>
+          <div>
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              size="lg"
+              className={activeNav === 3 ? "nav-item active" : "nav-item"}
+            />
+          </div>
+        </Link>
+        {/* 칼럼 */}
+        <Link to="/column" className="nav-link" onClick={() => setActiveNav(4)}>
+          <div>
+            <FontAwesomeIcon
+              icon={faFileLines}
+              size="lg"
+              className={activeNav === 4 ? "nav-item active" : "nav-item"}
+            />
+          </div>
+        </Link>
+        {/* MY */}
+        <Link
+          to="/profile"
+          className="nav-link"
+          onClick={() => setActiveNav(5)}
+        >
+          <div>
+            <FontAwesomeIcon
+              icon={faUser}
+              size="lg"
+              className={activeNav === 5 ? "nav-item active" : "nav-item"}
+            />
+          </div>
+        </Link>
+      </nav>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default BottomNav;
