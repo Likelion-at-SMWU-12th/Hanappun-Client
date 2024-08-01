901374c2b64c0ceacc5772d2ffffba3cbe732596
hyhy-j <lhjlhj10088@gmail.com>
2024-08-01 12:34:07 +0900

    [Feat] 병원 정보 및 예약 js 생성

--- hancare/src/pages/hospital/MapMain.js
+++ hancare/src/pages/hospital/MapMain.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const MapMain = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <h2>한의원</h2>
+    </Container>
+  );
 };
 
+const Container = styled.div`
+  display: flex;
+  background-color: black;
+  height: 100vh;
+
+  h2 {
+    color: white;
+    margin: 30px auto;
+  }
+`;
 export default MapMain;
