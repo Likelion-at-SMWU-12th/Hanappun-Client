7b8767f4c576fcc2d15d55c898571cb610f008c1
hyhy-j <lhjlhj10088@gmail.com>
2024-08-02 01:37:41 +0900

    [Feat] 특정 한의원 소개 및 후기 페이지 구현 완료

--- hancare/src/pages/hospital/HospitalHome.js
+++ hancare/src/pages/hospital/HospitalHome.js
@@ -29,12 +29,42 @@ const HospitalHome = () => {
   };
   const closeReservateModal = () => setReservateOpen(false);
 
+  // 더보기 버튼 관련, 돌아가기 버튼 관련
+  const [MorebtnClick, setMorebtnClick] = useState(false);
+  const handleMorebtnClick = () => {
+    setMorebtnClick(!MorebtnClick);
+  };
+
+  // 체크박스 피드백 관련
+  const [feedback, setFeedback] = useState({
+    clean: false,
+    medicine: false,
+    manage: false,
+    doctor: false,
+  });
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [name]: checked,
+    }));
+  };
+
+  // 리뷰 코멘트 작성 관련
+  const [comment, setComment] = useState("");
+  const onChangeComment = (e) => {
+    setComment(e.target.value);
+  };
+
   const BackButton = () => {
     navigate(-1);
   };
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
+  };
+  const ReviewSubmit = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -114,7 +144,197 @@ const HospitalHome = () => {
             <SubTitle>사진</SubTitle>
           </>
         ) : (
-          <h2>후기</h2>
+          <>
+            {!MorebtnClick ? (
+              <>
+                <SubTitle2>이런 점이 좋았어요</SubTitle2>
+                <ReviewWrapper>
+                  <ReviewOption>
+                    <img src="/images/review_nice.png"></img>
+                    <p>{item.reviewtext}</p>
+                  </ReviewOption>
+                  <Reviewnum>
+                    <p>{item.reviewnum}</p>
+                    <img src="/images/reviewnumimg.png" alt="num"></img>
+                  </Reviewnum>
+                </ReviewWrapper>
+                <ReviewWrapper>
+                  <ReviewOption>
+                    <img src="/images/review_heart.png"></img>
+                    <p>건강 관리에 철저해요</p>
+                  </ReviewOption>
+                  <Reviewnum>
+                    <p>79</p>
+                    <img src="/images/reviewnumimg.png" alt="num"></img>
+                  </Reviewnum>
+                </ReviewWrapper>
+                <ReviewWrapper>
+                  <ReviewOption>
+                    <img src="/images/review_clean.png"></img>
+                    <p>시설이 쾌적해요</p>
+                  </ReviewOption>
+                  <Reviewnum>
+                    <p>999+</p>
+                    <img src="/images/reviewnumimg.png" alt="num"></img>
+                  </Reviewnum>
+                </ReviewWrapper>
+                <SubTitle2>더 많은 이야기</SubTitle2>
+                <StoryBox>
+                  <p>
+                    <Bold>김눈송님</Bold>
+                    <br />
+                    김수련 원장님 매번 친절하게 상담해주셔서 좋았어요! 요즘
+                    감기도 걸리고 몸이 많이 안좋았는데 한의원 다니면서 효과도
+                    확실히 봐요ㅎㅎ
+                    <br />
+                    <Bold>#의료진, 직원이 친절해요 #건강관리에 철저해요</Bold>
+                  </p>
+                </StoryBox>
+                <StoryBox>
+                  <p>
+                    <Bold>파송송님</Bold>
+                    <br />
+                    매번 소화가 잘 안되서 문제였는데, 진료 받으면서 식습관을 잘
+                    고치게 되었던 것같아요! 매번 한약도 잘 복용하면서 한의원
+                    다니고 있습니다.
+                    <br />
+                    <Bold>#의료진, 직원이 친절해요 #건강관리에 철저해요</Bold>
+                  </p>
+                </StoryBox>
+                <MoreBtn onClick={handleMorebtnClick}>더보기</MoreBtn>
+                <SubTitle2>나의 이야기</SubTitle2>
+                <form>
+                  <ReviewQuest>
+                    <img src="/images/puplespot.png"></img>
+                    <h3>어떤 점이 좋았나요?</h3>
+                  </ReviewQuest>
+                  <CheckboxWrapper>
+                    <CheckboxLabel>
+                      <Checkbox
+                        type="checkbox"
+                        name="clean"
+                        checked={feedback.clean}
+                        onChange={handleCheckboxChange}
+                      />
+                      시설이 쾌적해요
+                    </CheckboxLabel>
+                  </CheckboxWrapper>
+                  <CheckboxWrapper>
+                    <CheckboxLabel>
+                      <Checkbox
+                        type="checkbox"
+                        name="medicine"
+                        checked={feedback.medicine}
+                        onChange={handleCheckboxChange}
+                      />
+                      약 처방이 잘 맞아요
+                    </CheckboxLabel>
+                  </CheckboxWrapper>
+                  <CheckboxWrapper>
+                    <CheckboxLabel>
+                      <Checkbox
+                        type="checkbox"
+                        name="manage"
+                        checked={feedback.manage}
+                        onChange={handleCheckboxChange}
+                      />
+                      건강 관리에 철저해요
+                    </CheckboxLabel>
+                  </CheckboxWrapper>
+                  <CheckboxWrapper>
+                    <CheckboxLabel>
+                      <Checkbox
+                        type="checkbox"
+                        name="doctor"
+                        checked={feedback.doctor}
+                        onChange={handleCheckboxChange}
+                      />
+                      의료진, 직원이 친절해요
+                    </CheckboxLabel>
+                  </CheckboxWrapper>
+                  <ReviewQuest>
+                    <img src="/images/puplespot.png"></img>
+                    <h3>숙멋사님의 이야기를 들려주세요</h3>
+                  </ReviewQuest>
+                  <CommentWrapper>
+                    <CommentBox
+                      type="text"
+                      placeholder="나의 이야기 남기기"
+                      value={comment}
+                      onChange={onChangeComment}
+                    />
+                    <ReviewSubmitBtn type="submit" onClick={ReviewSubmit}>
+                      입력
+                    </ReviewSubmitBtn>
+                  </CommentWrapper>
+                </form>
+              </>
+            ) : (
+              <>
+                <SubTitle2>더 많은 이야기</SubTitle2>
+                <StoryBox>
+                  <p>
+                    <Bold>김눈송님</Bold>
+                    <br />
+                    김수련 원장님 매번 친절하게 상담해주셔서 좋았어요! 요즘
+                    감기도 걸리고 몸이 많이 안좋았는데 한의원 다니면서 효과도
+                    확실히 봐요ㅎㅎ
+                    <br />
+                    <Bold>#의료진, 직원이 친절해요 #건강관리에 철저해요</Bold>
+                  </p>
+                </StoryBox>
+                <StoryBox>
+                  <p>
+                    <Bold>파송송님</Bold>
+                    <br />
+                    매번 소화가 잘 안되서 문제였는데, 진료 받으면서 식습관을 잘
+                    고치게 되었던 것같아요! 매번 한약도 잘 복용하면서 한의원
+                    다니고 있습니다.
+                    <br />
+                    <Bold>#의료진, 직원이 친절해요 #건강관리에 철저해요</Bold>
+                  </p>
+                </StoryBox>
+                <StoryBox>
+                  <p>
+                    <Bold>김명숙님</Bold>
+                    <br />
+                    우리 딸이 알려줘서 한의원 방문했습니다^^ 원장님께서 잘
+                    진찰해주셔서 더 신뢰가 가네요. 앞으로도 자주 찾아갈 것
+                    같아요
+                    <br />
+                    <Bold>
+                      #처방약이 좋아요 #건강관리에 철저해요 #의료진, 직원이
+                      친절해요
+                    </Bold>
+                  </p>
+                </StoryBox>
+                <StoryBox>
+                  <p>
+                    <Bold>푸른하늘님</Bold>
+                    <br />
+                    한의원이 좋아요
+                    <br />
+                    <Bold>#건강관리에 철저해요</Bold>
+                  </p>
+                </StoryBox>
+                <StoryBox>
+                  <p>
+                    <Bold>Bubble님</Bold>
+                    <br />
+                    교통사고가 갑자기 나서 근처 한의원으로 급하게 찾았는데 잘
+                    회복하는 중이에요ㅠㅠ 좋은 한의원 잘 찾은 것 같아서 주변에
+                    추천하고 다닙니다.
+                    <br />
+                    <Bold>
+                      #처방약이 좋아요 #건강관리에 철저해요 #의료진, 직원이
+                      친절해요
+                    </Bold>
+                  </p>
+                </StoryBox>
+                <MoreBtn onClick={handleMorebtnClick}>돌아가기</MoreBtn>
+              </>
+            )}
+          </>
         )}
       </Detail>
       {isMyModalOpen && (
@@ -141,6 +361,7 @@ const Container = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
 `;
 
 const Title = styled.div`
@@ -211,7 +432,7 @@ const Bold = styled.span`
 
 const ReservateBtn = styled.button`
   position: absolute;
-  top: 195px;
+  top: 192px;
   right: 65px;
   background-color: white;
   color: black;
@@ -227,13 +448,16 @@ const ReservateBtn = styled.button`
 
 const Detail = styled.div`
   background-color: #f7f7f7;
-  height: 100vh;
   width: 100%;
   border-top-right-radius: 25px;
   border-top-left-radius: 25px;
   margin-top: 15px;
   text-align: center;
   white-space: pre-line;
+  overflow-y: auto;
+  height: 100vh;
+  padding-bottom: 50px; /* 아래쪽 여백 추가 */
+  flex: 1;
 `;
 const DetailMenu = styled.div`
   display: flex;
@@ -296,6 +520,140 @@ const DoctorBox = styled.div`
 `;
 const Doctor2 = styled.img`
   width: 80px !important;
+`;
+
+const SubTitle2 = styled.h3`
+  color: #7350ff;
+  margin-top: 30px;
+`;
+
+const ReviewWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ReviewOption = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 8px #cecece;
+  width: 250px;
+  border-radius: 20px;
+  position: relative;
+  margin-right: 18px;
+  margin-left: 30px;
+
+  p {
+    font-size: 13px;
+    text-align: center;
+  }
+  img {
+    width: 12%;
+    position: absolute;
+    left: 20px;
+  }
+`;
+const Reviewnum = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  p {
+    font-size: 15px;
+    color: #7350ff;
+  }
+  img {
+    display: block;
+    text-align: right;
+    width: 18px;
+    height: auto;
+    margin-left: 8px;
+    position: absolute;
+    left: 35px;
+  }
+`;
+const StoryBox = styled.div`
+  box-shadow: 0 0 8px #cecece;
+  width: 330px;
+  margin: 10px auto;
+  padding: 5px 15px;
+  border-radius: 10px;
+  p {
+    font-size: 13px;
+    text-align: left;
+  }
+`;
+
+const MoreBtn = styled.button`
+  background: none;
+  border: none;
+  color: #7c7c7c;
+  cursor: pointer;
+  margin: 0 auto;
+  font-size: 15px;
+  font-weight: 600;
+`;
+
+const ReviewQuest = styled.div`
+  display: flex;
+  align-items: center;
+  h3 {
+    text-align: left;
+    margin-left: -10px;
+    font-size: 15px;
+  }
+  img {
+    width: 15px;
+    margin: 22px;
+  }
+`;
+
+const CheckboxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+const CheckboxLabel = styled.label`
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+  color: black;
+  font-size: 14px;
+
+  input {
+    margin-right: 10px;
+    zoom: 1.5;
+    border-color: #7350ff;
+  }
+`;
+
+const Checkbox = styled.input`
+  accent-color: #7350ff;
+`;
+const CommentWrapper = styled.div`
+  position: relative;
+`;
+const CommentBox = styled.textarea`
+  background-color: #eceef0;
+  border: none;
+  border-radius: 30px;
+  width: 320px;
+  height: 80px;
+  padding: 20px;
+  resize: none;
+  &::placeholder {
+  }
+`;
+
+const ReviewSubmitBtn = styled.button`
+  background: none;
+  border: none;
+  color: #7350ff;
+  display: block;
+  position: absolute;
+  top: 75px;
+  right: 50px;
+  font-weight: bolder;
+  cursor: pointer;
 `;
 
 export default HospitalHome;
