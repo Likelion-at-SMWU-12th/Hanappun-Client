74e5609aca30aeb608f453808506ce735a8f4f6b
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-08-03 21:27:22 +0900

    Chore : App.js 주석

--- hancare/src/App.js
+++ hancare/src/App.js
@@ -2,25 +2,36 @@ import "./App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Startpage from "./pages/account/Startpage";
-import Signup from "./pages/account/Signup";
-import Login from "./pages/account/Login";
-import Mainpage from "./pages/Mainpage";
-import ColumnMain from "./pages/Columns/ColumnMain";
-import Profile from "./pages/mypage/Profile";
-import Cancel from "./pages/mypage/Cancel";
-import CalendarMain from "./pages/calendar/CalendarMain";
-import MapMain from "./pages/hospital/MapMain";
-import OurCareStart from "./pages/ourcare/OurCareStart";
-import ColumnPancreotonia from "./pages/Columns/ColumnPancreotonia";
-import ColumnMoreKM1 from "./pages/Columns/ColumnMoreKM1";
-import MyCON from "./pages/myCON/MyCON";
-import ChooseMyCON from "./pages/myCON/ChooseMyCON";
-import SelfTest from "./pages/myCON/SelfTest";
-import SelfTestResult from "./pages/myCON/SelfTestResult";
-import EditMyCON from "./pages/myCON/EditMyCON";
-import OurCareFamily from "./pages/ourcare/OurCareFamily";
-import OurCareProfile from "./pages/ourcare/OurCareProfile";
-import HospitalHome from "./pages/hospital/HospitalHome";
+// import Signup from "./pages/account/Signup";
+// import Login from "./pages/account/Login";
+// import Mainpage from "./pages/Mainpage";
+// import ColumnMain from "./pages/Columns/ColumnMain";
+// import Profile from "./pages/mypage/Profile";
+// import Cancel from "./pages/mypage/Cancel";
+// import CalendarMain from "./pages/calendar/CalendarMain";
+// import MapMain from "./pages/hospital/MapMain";
+// import OurCareStart from "./pages/ourcare/OurCareStart";
+// import ColumnPancreotonia from "./pages/Columns/eight_CON/ColumnPancreotonia";
+// import ColumnHepatonia from "./pages/Columns/eight_CON/ColumnHepatonia";
+// import ColumnCholecystonia from "./pages/Columns/eight_CON/ColumnCholecystonia";
+// import ColumnGastrotonia from "./pages/Columns/eight_CON/ColumnGastrotonia";
+// import ColumnPulmotonia from "./pages/Columns/eight_CON/ColumnPulmotonia";
+// import ColumnColonotonia from "./pages/Columns/eight_CON/ColumnColonotonia";
+// import ColumnRenotonia from "./pages/Columns/eight_CON/ColumnRenotonia";
+// import ColumnVesicotonia from "./pages/Columns/eight_CON/ColumnVesicotonia";
+// import ColumnMoreKM1 from "./pages/Columns/moreKM/ColumnMoreKM1";
+// import ColumnMoreKM2 from "./pages/Columns/moreKM/ColumnMoreKM2";
+// import ColumnMoreKM3 from "./pages/Columns/moreKM/ColumnMoreKM3";
+// import ColumnMoreKM4 from "./pages/Columns/moreKM/ColumnMoreKM4";
+// import QmyCON from "./pages/myCON/QmyCON";
+// import MyCON from "./pages/myCON/MyCON";
+// import ChooseMyCON from "./pages/myCON/ChooseMyCON";
+// import SelfTest from "./pages/myCON/SelfTest";
+// import SelfTestResult from "./pages/myCON/SelfTestResult";
+// import EditMyCON from "./pages/myCON/EditMyCON";
+// import OurCareFamily from "./pages/ourcare/OurCareFamily";
+// import OurCareProfile from "./pages/ourcare/OurCareProfile";
+// import HospitalHome from "./pages/hospital/HospitalHome";
 
 const Wrapper = styled.div`
   width: 400px;
@@ -36,24 +47,35 @@ const Wrapper = styled.div`
 `;
 
 function App() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
     <Wrapper>
       <Routes>
         <Route path="/" element={<Startpage />} />
-        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mainpage/" element={<Mainpage />} />
         <Route path="/calendar" element={<CalendarMain />} />
-        <Route path="/ourcare/" element={<OurCareStart />} />
+        <Route path="/ourcare/" element={<OurCareStart />} /> */}
         {/*마이페이지*/}
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/cancel" element={<Cancel />} />
+        {/* <Route path="/profile" element={<Profile />} />
+        <Route path="/cancel" element={<Cancel />} /> */}
         {/*칼럼 -- 더 추가 예정 ... */}
-        <Route path="/column" element={<ColumnMain />} />
+        {/* <Route path="/column" element={<ColumnMain />} />
         <Route path="/column/Pancreotonia" element={<ColumnPancreotonia />} />
+        <Route path="/column/Hepatonia" element={<ColumnHepatonia />} />
+        <Route path="/column/Cholecystonia" element={<ColumnCholecystonia />} />
+        <Route path="/column/Gastrotonia" element={<ColumnGastrotonia />} />
+        <Route path="/column/Pulmotonia" element={<ColumnPulmotonia />} />
+        <Route path="/column/Colonotonia" element={<ColumnColonotonia />} />
+        <Route path="/column/Renotonia" element={<ColumnRenotonia />} />
+        <Route path="/column/Vesicotonia" element={<ColumnVesicotonia />} />
         <Route path="/column/moreKM1" element={<ColumnMoreKM1 />} />
+        <Route path="/column/moreKM2" element={<ColumnMoreKM2 />} />
+        <Route path="/column/moreKM3" element={<ColumnMoreKM3 />} />
+        <Route path="/column/moreKM4" element={<ColumnMoreKM4 />} /> */}
         {/*나의 체질*/}
+        {/* <Route path="/q_myCON" element={<QmyCON />} />
         <Route path="/myCON" element={<MyCON />} />
         <Route path="/myCON/selfTest" element={<SelfTest />} />
         <Route path="/myCON/selfTestResult" element={<SelfTestResult />} />
@@ -61,11 +83,11 @@ function App() {
         <Route path="/myCON/editMyCON" element={<EditMyCON />} />
 
         <Route path="/ourcare/family/" element={<OurCareFamily />} />
-        <Route path="/ourcare/family/:id" element={<OurCareProfile />} />
+        <Route path="/ourcare/family/:id" element={<OurCareProfile />} /> */}
 
         {/* 한의원 */}
-        <Route path="/map" element={<MapMain />} />
-        <Route path="/map/:id" element={<HospitalHome />} />
+        {/* <Route path="/map" element={<MapMain />} />
+        <Route path="/map/:id" element={<HospitalHome />} /> */}
       </Routes>
     </Wrapper>
   );
